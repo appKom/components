20e0321db0238b790868ca639d14a7a0cc83c582
fredrir <fhansteen@gmail.com>
2024-11-01 13:43:43 +0100

    add a component

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
@@ -25,6 +26,11 @@ export default {
     resolve(),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
+    postcss({
+      extract: true,
+      minimize: true,
+      sourceMap: true,
+    }),
     terser(),
   ],
 };
